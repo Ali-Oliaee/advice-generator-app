3d18321624374870055be80e82be3d53f9ff2049
james <ali_o@felixin.io>
2023-08-26 19:42:21 +0330

    add loading element

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const changeAdvice = () => {
-  fetch("https://api.adviceslip.com/advice").then((response) => {
-    response.json().then((data) => {
-      document.querySelector(".advice").innerHTML =
-        "&#8220;" + data.slip.advice + "&#8221;";
-      document.querySelector("#advice-id").innerHTML = "#" + data.slip.id;
-    });
-  });
-};
+  const loadingElement = document.getElementById("loading")
+  const adviceElement = document.getElementById("advice")
+  const adviceId = document.getElementById("advice-id")
+  loadingElement.classList.remove("hide")
+  adviceElement.classList.add("hide")
+  adviceId.classList.add("hide")
+  fetch("https://api.adviceslip.com/advice")
+    .then((response) => {
+      response.json().then((data) => {
+        document.querySelector(".advice").innerHTML =
+          "&#8220;" + data.slip.advice + "&#8221;"
+        document.querySelector("#advice-id").innerHTML = "#" + data.slip.id
+      })
+    })
+    .finally(() => {
+      loadingElement.classList.add("hide")
+      adviceElement.classList.remove("hide")
+      adviceId.classList.remove("hide")
+    })
+}
